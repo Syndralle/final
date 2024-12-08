f955bb8db8867119d7f5375be691863337f44672
Syndralle <997989453@qq.com>
2024-12-08 10:20:10 +0800

    update curve

--- curve_.py
+++ curve_.py
@@ -34,12 +34,10 @@ except FileNotFoundError:
     exit()
 
 # iou
-# datas = [data_ciou, data_eiou, data_siou, data_wiou]
-# labels = ['ciou', 'eiou', 'siou', 'wiou']
+datas = [data_ciou, data_eiou, data_siou, data_wiou]
+labels = ['ciou', 'eiou', 'siou', 'wiou']
 
 # ablation
-datas = [data_v8s, data_our]
-labels = ['yolov8s', 'our model']
 
 def smooth_data(data, column, window_size):
     return data[column].rolling(window=window_size, center=False).mean()
@@ -78,18 +76,20 @@ def plot_curve(datas, x_column, y_columns, title, xlabel, ylabel, ranger, loc, s
 
 
 # iou
-# plot_curve(datas, 'epoch', ['train/box_loss'], 'train/box loss over epochs', 'epoch', 'loss', [280, 300, 0.75, 0.9], ['lower left', 1], 'v8n_train_box_loss.png')
-# plot_curve(datas, 'epoch', ['val/box_loss'], 'val/box loss over epochs', 'epoch', 'loss', [280, 300, 0.8, 0.9], ['lower left', 1], 'v8n_val_box_loss.png')
-# plot_curve(datas, 'epoch', ['metrics/precision(B)'], 'precision over epochs', 'epoch', 'loss', [280, 300, 0.85, 0.95], ['lower right', 'center'], 'v8n_precision.png')
-# plot_curve(datas, 'epoch', ['metrics/recall(B)'], 'recall over epochs', 'epoch', 'loss', [280, 300, 0.87, 0.95], ['lower right', 'center'], 'v8n_recall.png')
-# plot_curve(datas, 'epoch', ['metrics/mAP50(B)'], 'mAP50 over epochs', 'epoch', 'mAP50(%)', [280, 300, 0.95, 0.985], ['lower right', 'center'], 'v8n_map50.png')
-# plot_curve(datas, 'epoch', ['metrics/mAP50-95(B)'], 'mAP50-95 over epochs', 'epoch', 'mAP50-95', [280, 300, 0.68, 0.74], ['lower right', 'center'], 'v8n_map50_95.png')
+plot_curve(datas, 'epoch', ['train/box_loss'], 'train/box loss over epochs', 'epoch', 'loss', [280, 300, 0.75, 0.9], ['lower left', 1], 'v8n_train_box_loss.png')
+plot_curve(datas, 'epoch', ['val/box_loss'], 'val/box loss over epochs', 'epoch', 'loss', [280, 300, 0.8, 0.9], ['lower left', 1], 'v8n_val_box_loss.png')
+plot_curve(datas, 'epoch', ['metrics/precision(B)'], 'precision over epochs', 'epoch', 'loss', [280, 300, 0.85, 0.95], ['lower right', 'center'], 'v8n_precision.png')
+plot_curve(datas, 'epoch', ['metrics/recall(B)'], 'recall over epochs', 'epoch', 'loss', [280, 300, 0.87, 0.95], ['lower right', 'center'], 'v8n_recall.png')
+plot_curve(datas, 'epoch', ['metrics/mAP50(B)'], 'mAP@50 over epochs', 'epoch', 'mAP@50', [280, 300, 0.95, 0.985], ['lower right', 'center'], 'v8n_map50.png')
+plot_curve(datas, 'epoch', ['metrics/mAP50-95(B)'], 'mAP@50:95 over epochs', 'epoch', 'mAP@50:95', [280, 300, 0.68, 0.74], ['lower right', 'center'], 'v8n_map50_95.png')
+#
 
-
-
-plot_curve(datas, 'epoch', ['train/box_loss'], 'train/box loss over epochs', 'epoch', 'loss', [280, 300, 0.75, 0.9], ['upper right', 1], 'ablation_train_box_loss.png', False, 30)
-plot_curve(datas, 'epoch', ['val/box_loss'], 'val/box loss over epochs', 'epoch', 'loss', [280, 300, 0.8, 0.9], ['upper right', 1], 'ablation_val_box_loss.png', False, 30)
-plot_curve(datas, 'epoch', ['metrics/precision(B)'], 'precision over epochs', 'epoch', 'loss', [280, 300, 0.85, 0.95], ['lower right', 'center'], 'ablation_precision.png', False, 3)
-plot_curve(datas, 'epoch', ['metrics/recall(B)'], 'recall over epochs', 'epoch', 'loss', [280, 300, 0.87, 0.95], ['lower right', 'center'], 'ablation_recall.png', False, 5)
-plot_curve(datas, 'epoch', ['metrics/mAP50(B)'], 'mAP50 over epochs', 'epoch', 'mAP50(%)', [280, 300, 0.95, 0.985], ['lower right', 'center'], 'ablation_map50.png', False, 50)
-plot_curve(datas, 'epoch', ['metrics/mAP50-95(B)'], 'mAP50-95 over epochs', 'epoch', 'mAP50-95', [280, 300, 0.68, 0.74], ['lower right', 'center'], 'ablation_map50_95.png', False, 50)
+# datas = [data_v8s, data_our]
+# labels = ['yolov8s', 'our model']
+#
+# plot_curve(datas, 'epoch', ['train/box_loss'], 'train/box loss over epochs', 'epoch', 'loss', [280, 300, 0.75, 0.9], ['upper right', 1], 'ablation_train_box_loss.png', False, 30)
+# plot_curve(datas, 'epoch', ['val/box_loss'], 'val/box loss over epochs', 'epoch', 'loss', [280, 300, 0.8, 0.9], ['upper right', 1], 'ablation_val_box_loss.png', False, 30)
+# plot_curve(datas, 'epoch', ['metrics/precision(B)'], 'precision over epochs', 'epoch', 'loss', [280, 300, 0.85, 0.95], ['lower right', 'center'], 'ablation_precision.png', False, 3)
+# plot_curve(datas, 'epoch', ['metrics/recall(B)'], 'recall over epochs', 'epoch', 'loss', [280, 300, 0.87, 0.95], ['lower right', 'center'], 'ablation_recall.png', False, 5)
+# plot_curve(datas, 'epoch', ['metrics/mAP50(B)'], 'mAP@50 over epochs', 'epoch', 'mAP@50', [280, 300, 0.95, 0.985], ['lower right', 'center'], 'ablation_map50.png', False, 50)
+# plot_curve(datas, 'epoch', ['metrics/mAP50-95(B)'], 'mAP@50:95 over epochs', 'epoch', 'mAP@50:95', [280, 300, 0.68, 0.74], ['lower right', 'center'], 'ablation_map50_95.png', False, 50)
